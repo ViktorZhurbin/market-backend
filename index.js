d3ab1cb31acec9da8d0b080e2067914acb844c1c
Viktor Zhurbin <vzhurbin@gmail.com>
2020-10-10 18:21:45 +0300

    add env file

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 const adminRoutes = require('./routes/admin');
 
-const MONGODB_URI =
-  'mongodb+srv://vzhurbin:3ii25kiQRh0lKinB@market.ffdmw.mongodb.net/market?retryWrites=true&w=majority';
+('mongodb+srv://vzhurbin:3ii25kiQRh0lKinB@market.ffdmw.mongodb.net/market?retryWrites=true&w=majority');
 
 const app = express();
 
@@ -16,7 +14,7 @@ app.use(bodyParser.json());
 app.use('/admin', adminRoutes);
 
 mongoose
-  .connect(MONGODB_URI, {
+  .connect(process.env.MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   })
